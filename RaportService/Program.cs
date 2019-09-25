f84e09492272ca276b239c91268e62e5f19e8767
Andrzej Hosz <andrzej.hosz@gmail.com>
2019-09-25 19:23:04 +0200

    Report Service

---
+++ RaportService/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ReportService.Models;
+
+namespace ReportService
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var sellDateEnd = DateTime.Now;
+			var productsSalesData = new List<Tuple<Product, int>>
+			{
+				Tuple.Create(new Product("Browar", 5), 100),
+				Tuple.Create(new Product("Whisky", 100), 10),
+				Tuple.Create(new Product("Rum", 30), 7),
+				Tuple.Create(new Product("Wódka", 23), 203),
+				Tuple.Create(new Product("Brandy", 43), 16),
+				Tuple.Create(new Product("Piwo bezalkoholowe", 2), 0)
+			};
+
+            var reportService = new ReportService();
+
+			var reportFilepath = reportService.CreateProductsReport(sellDateEnd, productsSalesData);
+
+			var fileInfo = new FileInfo(reportFilepath);
+
+			Console.WriteLine($"File created ? - {fileInfo.Exists}");
+
+			if (fileInfo.Exists)
+			{
+				Console.WriteLine($"Length of file in bites : - {fileInfo.Length}");
+
+				if (fileInfo.Length == 0)
+				{
+					Console.WriteLine("Plik pusty - coś nie tak ;)");
+				}
+
+				using (var streamReader = fileInfo.OpenText())
+				{
+					var reportData = streamReader.ReadToEnd();
+
+					Console.Write(reportData);
+				}
+			}
+			else
+			{
+				Console.WriteLine("Coś poszło nie tak");
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ RaportService/ReportService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ReportService.Models;
+
+namespace ReportService
+{
+    public class ReportService
+    {
+        public string CreateProductsReport(
+            DateTime saleDateEnd,
+            List<Tuple<Product, int>> productsWithSalesCount)
+        {
+            var filePath = "Reports" + Guid.NewGuid() + ".txt";
+
+            var writeStream = new StreamWriter(File.Create(filePath));
+
+            writeStream.WriteLine($"Sales raport start : {saleDateEnd.Date.AddDays(-7).ToShortDateString()}" + $"\nSales raport end : {saleDateEnd.ToShortDateString()}\n");
+
+            foreach (var tuple in productsWithSalesCount)
+            {
+                writeStream.WriteLine($"Product : {tuple.Item1.Name}\nPrice : {tuple.Item1.Price}" + $"\nSold :{tuple.Item2}\n ");
+            }
+            writeStream.Close();
+            return filePath;
+        }
+    }
+}
